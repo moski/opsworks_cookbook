b9688ffcecaf3d1740f3c0b5f31ad2760f7f28de
Moayyad Faris <moayyadzf@gmail.com>
2016-11-22 12:35:00 +0200

    add AWS_S3_BUCKET_URL

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -36,6 +36,7 @@ node[:deploy].each do |application, deploy|
       :s3_access_key    => (deploy[:aws][:s3_access_key] rescue nil),
       :s3_secret_key    => (deploy[:aws][:s3_secret_key] rescue nil),
       :s3_bucket    => (deploy[:aws][:s3_bucket] rescue nil),
+      :s3_bucket_url    => (deploy[:aws][:s3_bucket_url] rescue nil),
 
       :redis_url    => (deploy[:aws][:redis][:url] rescue nil),
 
