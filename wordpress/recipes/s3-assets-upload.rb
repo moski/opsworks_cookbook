6573fcb38f461744f5a1443619b2945786454a8f
Moayyad Faris <moayyadzf@gmail.com>
2016-11-28 17:23:52 +0200

    s3 assets upload hook

---
+++ wordpress/recipes/s3-assets-upload.rb
@@ -0,0 +1,25 @@
+#upload dir to s3
+
+node[:deploy].each do |application, deploy|
+	Chef::Log.info("Uploading Assests to S3 Bucket #{application}...")
+	deploy = node[:deploy][application]
+    
+
+	workdir = deploy[:aws][:workdir]
+	aws_key = deploy[:aws][:s3_access_key]
+	aws_secret = deploy[:aws][:s3_secret_key]
+	bucket_name = deploy[:aws][:s3_bucket]
+
+	template "/usr/local/bin/aws" do
+	  source "aws"
+	  mode 0660
+	  group deploy[:group]
+	  owner deploy[:user]
+	end
+
+ 
+	 execute "cd #{workdir} && 
+	  EC2_ACCESS_KEY=#{aws_key} EC2_SECRET_KEY=#{aws_secret} /usr/local/bin/aws put #{bucket_name}/wp-content/temp"
+
+end
+
