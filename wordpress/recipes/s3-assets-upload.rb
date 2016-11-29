eafa24d76d52f7682cdb834c1544ef358cda4ab3
Moayyad Faris <moayyadzf@gmail.com>
2016-11-29 23:03:03 +0200

    include assets files only

--- wordpress/recipes/s3-assets-upload.rb
+++ wordpress/recipes/s3-assets-upload.rb
@@ -17,14 +17,15 @@ node[:deploy].each do |application, deploy|
 	aws_secret = (deploy[:aws][:s3_secret_key] rescue nil)
 	bucket_name = (deploy[:aws][:s3_bucket] rescue nil)
 	s3_region = (deploy[:aws][:s3_region] rescue nil)
+	s3_assets_dir = (deploy[:aws][:s3_assets_dir] rescue nil)
 	
 
  	Chef::Log.info("Start on sync #{workdir} to #{bucket_name}")
  
-	execute "export AWS_DEFAULT_REGION=#{s3_region} &&
+	execute 'export AWS_DEFAULT_REGION=#{s3_region} &&
 	 export AWS_ACCESS_KEY_ID=#{aws_key} && cd #{workdir} && 
 	 export AWS_SECRET_ACCESS_KEY=#{aws_secret} &&
-	  aws s3 sync . s3://#{bucket_name}/tmp"
+	  aws s3 sync . s3://#{bucket_name}/#{s3_assets_dir} --acl public-read --exclude "*" --include "*.jpg" --include "*.png" --include "*.css" --include "*.js" --include "*.gif"'
 	 Chef::Log.info("End sync #{workdir} to #{bucket_name}")
 
 end
