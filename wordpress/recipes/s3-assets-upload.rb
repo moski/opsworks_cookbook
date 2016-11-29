41ef68fd07be5bf551bbc6395a8438bff3a6fd95
Moayyad Faris <moayyadzf@gmail.com>
2016-11-29 16:44:07 +0200

    using awscli

--- wordpress/recipes/s3-assets-upload.rb
+++ wordpress/recipes/s3-assets-upload.rb
@@ -2,6 +2,13 @@
 
 node[:deploy].each do |application, deploy|
 	Chef::Log.info("Uploading Assests to S3 Bucket #{application}...")
+
+	if defined?(deploy[:application_type]) && deploy[:application_type] != 'php'
+	    Chef::Log.info("Skipping Uploading Assests to S3 Bucket  application #{application} as it is not defined as #{deploy[:application_type]}")
+	    next
+    end
+    tmp = deploy
+
 	deploy = node[:deploy][application]
     
 
@@ -9,7 +16,7 @@ node[:deploy].each do |application, deploy|
 	aws_key = (deploy[:aws][:s3_access_key] rescue nil)
 	aws_secret = (deploy[:aws][:s3_secret_key] rescue nil)
 	bucket_name = (deploy[:aws][:s3_bucket] rescue nil)
-
+	s3_region = (deploy[:aws][:s3_region] rescue nil)
 	template "/usr/local/bin/aws" do
 	  source "aws"
 	  mode 0700
@@ -19,13 +26,12 @@ node[:deploy].each do |application, deploy|
 
  	Chef::Log.info("workdir = #{workdir}...")
  	Chef::Log.info("bucket_name = #{bucket_name}...")
-	Dir.glob(workdir) do |item|
-	  next if item == '.' or item == '..'
-	  # do work on real items
-	  execute "cd #{workdir} && 
-	  EC2_ACCESS_KEY=#{aws_key} EC2_SECRET_KEY=#{aws_secret} /usr/local/bin/aws put #{bucket_name}/wp-content/temp #{item}"
+ 	#execute "export AWS_ACCESS_KEY_ID=#{s3_access_key}"
+ 	#execute "export AWS_SECRET_ACCESS_KEY=#{s3_secret_key}"
+ 	#execute "export AWS_DEFAULT_REGION=#{s3_region}"
+	execute "cd #{workdir} && 
+	  aws sync .  s3://#{bucket_name}/tmp"
 	 
-	end
 	 
 end
 
