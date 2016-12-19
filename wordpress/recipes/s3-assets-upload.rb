8df394007d6f7571164aabdcb20c66c18d66ba5b
Moayyad Faris <moayyadzf@gmail.com>
2016-12-19 11:09:58 +0200

    create min.task file

--- wordpress/recipes/s3-assets-upload.rb
+++ wordpress/recipes/s3-assets-upload.rb
@@ -35,9 +35,11 @@ node[:deploy].each do |application, deploy|
 	  aws s3 sync . s3://#{bucket_name}/#{s3_assets_dir} --acl public-read --cache-control \"max-age=31536000\" --exclude \"*\" --include \"*.jpg\" --include \"*.png\" --include \"*.css\" --include \"*.js\" --include \"*.gif\" --include \"*.ttf\" --include \"*.woff\" --include \"*.woff2\" --include \"*.svg\" --include \"*.eot\""
 
 	 execute "export AWS_DEFAULT_REGION=#{s3_region} &&
-	 export AWS_ACCESS_KEY_ID=#{aws_key} && cd #{deploy[:deploy_to]}/current/#{s3_assets_dir} && 
+	 export AWS_ACCESS_KEY_ID=#{aws_key} && 
 	 export AWS_SECRET_ACCESS_KEY=#{aws_secret} &&
-	  aws s3 cp min.task s3://#{bucket_name}/#{s3_assets_dir}"
+	 cd #{deploy[:deploy_to]}/current/#{s3_assets_dir} && 
+	 echo $(date) >min.task &&
+	 aws s3 cp min.task s3://#{bucket_name}/#{s3_assets_dir}"
 	 Chef::Log.info("End sync #{s3_assets_dir} to #{bucket_name}")
 
 end
