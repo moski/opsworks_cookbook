029fdb3067ce052226ba8ba008f8ee39426029ca
Moayyad Faris <moayyadzf@gmail.com>
2017-10-02 17:35:43 +0300

    change varnish default storage to malloc

--- varnish/attributes/customize.rb
+++ varnish/attributes/customize.rb
@@ -11,7 +11,7 @@ default['varnish']['nfiles'] = 131072
 default['varnish']['memlock'] = 82000
 default['varnish']['instance'] = node['hostname']
 default['varnish']['listen_address'] = ''
-default['varnish']['listen_port'] = 6081
+default['varnish']['listen_port'] = 80
 default['varnish']['vcl_conf'] = 'default.vcl'
 default['varnish']['vcl_source'] = 'default.vcl.erb'
 default['varnish']['vcl_cookbook'] = nil
@@ -24,9 +24,12 @@ default['varnish']['ttl'] = '120'
 default['varnish']['min_threads'] ='5'
 default['varnish']['max_threads'] = '500'
 default['varnish']['thread_timeout'] = '300'
-default['varnish']['storage'] = 'file'
+#default['varnish']['storage'] = 'file'
 default['varnish']['storage_file'] = '/var/lib/varnish/$INSTANCE/varnish_storage.bin'
-default['varnish']['storage_size'] = '1G'
+#default['varnish']['storage_size'] = '1G'
+
+default['varnish']['storage'] = 'malloc'
+default['varnish']['storage_size'] = '256m'
 
 default['varnish']['backend_host'] = '127.0.0.1'
 default['varnish']['backend_port'] = '8080'
