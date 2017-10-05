4fc661df96e7cfe86914a099db4c6717e0e439b6
Moayyad Faris <moayyadzf@gmail.com>
2017-10-05 16:33:35 +0300

    re-enable s3-assests-upload recipe

--- wordpress/recipes/s3-assets-upload.rb
+++ wordpress/recipes/s3-assets-upload.rb
@@ -1,6 +1,6 @@
 #upload dir to s3
 
-=begin
+ 
 node[:deploy].each do |application, deploy|
 	Chef::Log.info("Uploading Assests to S3 Bucket #{application}...")
 
@@ -44,4 +44,4 @@ node[:deploy].each do |application, deploy|
 
 end
 
-=end
+ 
