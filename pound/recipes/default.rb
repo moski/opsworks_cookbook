9f0399c105314abe526de25ae70e9ac79225b898
Moayyad Faris <moayyadzf@gmail.com>
2017-10-16 14:08:57 +0300

    fix pound start service

--- pound/recipes/default.rb
+++ pound/recipes/default.rb
@@ -1,15 +1,9 @@
-service "pound" do
-  supports :restart => true
-  action [ :start ]
-end
 
-node[:deploy].each do |application, deploy|
- Chef::Log.info("Configuring Pound #{application}...")
- deploy = node[:deploy][application]
 
-  execute "openssl x509 -in /etc/apache2/ssl/#{deploy[:hostname]}.crt -out /etc/apache2/ssl/#{node[:pound][:cert]}.pem"
-  execute "openssl rsa -in /etc/apache2/ssl/#{deploy[:hostname]}.key >> /etc/apache2/ssl/#{node[:pound][:cert]}.pem"
-  template "/etc/pound/pound.cfg" do
+execute "openssl x509 -in /etc/apache2/ssl/#{node[:pound][:hostname]}.crt -out /etc/apache2/ssl/#{node[:pound][:cert]}.pem"
+execute "openssl rsa -in /etc/apache2/ssl/#{node[:pound][:hostname]}.key >> /etc/apache2/ssl/#{node[:pound][:cert]}.pem"
+
+template "/etc/pound/pound.cfg" do
       source "pound.cfg.erb"
       mode 0660
 
@@ -17,15 +11,22 @@ node[:deploy].each do |application, deploy|
         :cert         => (node[:pound][:cert] rescue nil),
         :https_backend_port  => (node[:pound][:https_backend_port] rescue nil),
         # Domain
-        :redirect_domain           => (deploy[:hostname]))
-  end
-  Chef::Log.info("Finished Creating Pound cfg...")
-  template "/etc/default/pound" do
+        :redirect_domain           => (node[:pound][:hostname]))
+end
+ 
+Chef::Log.info("Finished Creating Pound cfg...")
+
+template "/etc/default/pound" do
       source "pound-default.erb"
       mode 0660
 
       variables(:start => (node[:pound][:start] rescue nil))
       notifies :restart, "service[pound]"
-  end
-  
+end
+ 
+
+
+service "pound" do
+  supports :restart => true
+  action :start
 end
