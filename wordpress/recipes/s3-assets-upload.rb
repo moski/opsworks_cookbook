cee181ed05dddcb252d17c7a1b75e54101877388
Moayyad Faris <moayyadzf@gmail.com>
2018-12-27 15:35:16 +0200

    s3 assets upload

--- wordpress/recipes/s3-assets-upload.rb
+++ wordpress/recipes/s3-assets-upload.rb
@@ -20,21 +20,15 @@ node[:deploy].each do |application, deploy|
 	s3_assets_remote = (deploy[:aws][:s3_assets_remote] rescue nil)
 
  	Chef::Log.info("Start on sync #{s3_assets_dir} to #{bucket_name}")
- 	
+
  	execute "export AWS_DEFAULT_REGION=#{s3_region} &&
 	         export AWS_ACCESS_KEY_ID=#{aws_key} &&
 	         export AWS_SECRET_ACCESS_KEY=#{aws_secret} &&
 	         cd #{deploy[:deploy_to]}/current/#{s3_assets_dir} &&
-	aws s3 sync . s3://#{bucket_name}/#{s3_assets_remote} --acl public-read --cache-control \"max-age=31536000\" --exclude \"*\" --include \"*.jpg\" --include \"*.png\" --include \"*.css\" --include \"*.js\" --include \"*.gif\" --include \"*.mp4\" --include \"*.webm\" --include \"*.ogv\" --include \"*.ttf\" --include \"*.woff\" --include \"*.woff2\" --include \"*.svg\" --include \"*.eot\"
+	aws s3 sync . s3://#{bucket_name}/#{s3_assets_remote} --acl public-read --cache-control \"max-age=31536000\" --exclude \"*\" --include \"*.jpg\" --include \"*.png\" --include \"*.css\" --include \"*.js\" --include \"*.gif\" --include \"*.mp4\" --include \"*.webm\" --include \"*.ogv\" --include \"*.ttf\" --include \"*.woff\" --include \"*.woff2\" --include \"*.svg\" --include \"*.eot\" --include \"*.kml\" --include \"*.xlsx\" --include \"*.json\" --include \"*.ico\" --include \"*.xml\"
 	"
- 
-	 execute "export AWS_DEFAULT_REGION=#{s3_region} &&
-	 export AWS_ACCESS_KEY_ID=#{aws_key} && 
-	 export AWS_SECRET_ACCESS_KEY=#{aws_secret} &&
-	 cd #{deploy[:deploy_to]}/current/#{s3_assets_dir} && 
-	 echo $(date)::$(git log --format=%B -n 1) >min.task &&
-	 aws s3 cp min.task s3://#{bucket_name}/#{s3_assets_remote}"
-	 Chef::Log.info("End sync #{s3_assets_remote} to #{bucket_name}")
+
+	Chef::Log.info("End sync #{s3_assets_remote} to #{bucket_name}")
 
 end
 
